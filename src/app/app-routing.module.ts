3840f7502b1a850bf55416b81d3822c82345bf5a
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-01-16 11:13:15 -0500

    Refactor buttons comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CustomerNewComponent } from './features/components/customer/customer-new.component';
 import { CustomerAdminComponent } from './features/components/customer/customer-admin.component';
 import { CustomerEditComponent } from './features/components/customer/customer-edit.component';
+import { ButtonsComponent } from './features/components/buttons/buttons.component';
 
 const routes: Routes = [
   { path: 'customers', component: CustomerAdminComponent },
   { path: 'customer-new', component: CustomerNewComponent },
   { path: 'customer-edit/:id', component: CustomerEditComponent},
+  { path: 'buttons', component: ButtonsComponent },
   { path: '**', redirectTo: 'customer-new' }
 ];
 
--- src/app/features/feature.module.ts
+++ src/app/features/feature.module.ts
@@ -6,12 +6,14 @@ import { SharedModule } from '../shared/shared.module';
 import { CustomerNewComponent } from './components/customer/customer-new.component';
 import { CustomerAdminComponent } from './components/customer/customer-admin.component';
 import { CustomerEditComponent } from './components/customer/customer-edit.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
 
 @NgModule({
   declarations: [
     CustomerNewComponent,
     CustomerAdminComponent,
-    CustomerEditComponent
+    CustomerEditComponent,
+    ButtonsComponent
   ],
   imports: [
     CommonModule,
